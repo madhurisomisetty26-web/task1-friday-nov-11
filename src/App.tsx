1e83c5ce01289354e8434a9120694406c9cbf09c
madhurisomisetty26-web <madhurisomisetty26@gmail.com>
2025-11-14 07:27:13 -0600

    task2 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import Task1 from './components/Task1'
-import Task2 from './components/Task2'
+
 import Task3 from './components/Task3'
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
     <div>
       <h1>Friday Task</h1>
       <Task1/>
-      <Task2/>
+     
       <Task3/>
     </div>
   )
